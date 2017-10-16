180e22d48a7bbad8ed09147dbf7483d86ea3cc82
EJStensen <eirik.stensen95@gmail.com>
2017-10-17 01:56:13 +0200

    Ikke ferdig...

---
+++ src/Oving8/Heap.java
@@ -0,0 +1,73 @@
+package Oving8;
+
+public class Heap {
+	int length;
+	int node[];
+	
+	int over(int position) {
+		return (position - 1) >> 1;
+	}
+	
+	int left(int position) {
+		return (position << 1) + 1;
+	}
+	
+	int right(int position) {
+		return (position + 1) << 1;
+	}
+	
+	public void fix_heap(int position) {
+		int m = left(position);
+		if (m < length){
+			int h = m + 1;
+			if (h < length && node[h] > node[m]) m = h;
+			if (node[m] > node[position]){
+				swap(position, m);
+				fix_heap(m);
+			}
+		}
+	}
+	
+	private void swap(int i, int j){
+		Object temp = node[i];
+		node[i] = node[j];
+		node[j] = (int) temp;
+	}
+	
+	public void make_heap(){
+		int i = length / 2;
+		while (i --> 0) fix_heap(i);
+	}
+	
+	public int get_max(){
+		int max = node[0];
+		node[0] = node[--length];
+		fix_heap(0);
+		return max;
+	}
+	
+	public void priority_up(int position, int value){
+		int parentnode;
+		node[position] += value;
+		while (position > 0 && node[position] > node[parentnode=over(position)]){
+			swap(position, parentnode);
+			position = parentnode;
+		}
+	}
+	
+	public void set_in(int newnode){
+		int i = length++;
+		node[i] = newnode;
+		priority_up(i, 0);
+	}
+	
+	public void heapsort(){
+		make_heap();
+		int l = length;
+		while(length > 1){
+			int x = get_max();
+			node[length] = x;
+		}
+		length = l;
+	}
+}
